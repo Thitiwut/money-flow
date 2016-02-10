57bdfcce7e075c95acb29b305b8386585bb82364
Woraprat Jindain <woraprat.j@aware.local>
2016-02-10 14:38:56 +0700

    Update migration

--- database/migrations/2016_02_10_040809_create_database.php
+++ database/migrations/2016_02_10_040809_create_database.php
@@ -1,7 +1,7 @@
 <?php
 
-use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
 
 class CreateDatabase extends Migration
 {
@@ -12,7 +12,64 @@ class CreateDatabase extends Migration
      */
     public function up()
     {
-        //
+        Schema::create('User', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('username');
+            $table->string('email');
+            $table->string('password', 255);
+            $table->timestamps();
+        });
+
+        Schema::create('Plan', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('user_id');
+            $table->string('name');
+            $table->text('description');
+            $table->string('capital', 10);
+            $table->timestamps();
+        });
+
+        Schema::create('Monthly', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('plan_id');
+            $table->string('income', 10);
+            $table->string('status');
+            $table->timestamps();
+        });
+
+        Schema::create('Daily', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('monthly_id');
+            $table->date('date');
+            $table->string('expense');
+            $table->string('income');
+            $table->timestamps();
+        });
+
+        Schema::create('Finance', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('daily_id');
+            $table->integer('category_id');
+            $table->text('description');
+            $table->string('name');
+            $table->string('amount');
+            $table->integer('type');
+            $table->timestamps();
+        });
+
+        Schema::create('Restrict', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('plan_id');
+            $table->integer('category_id');
+            $table->string('exceed');
+            $table->timestamps();
+        });
+
+        Schema::create('Category', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('user_id');
+            $table->string('name');
+        });
     }
 
     /**

f62a7ff8d5ad7205a0758d8dee53e4a4bc4f6ea7
Thitiwut <thitiwut_s@gmail.com>
2016-02-29 22:11:59 +0700

    test

--- tests/ExampleTest.php
+++ tests/ExampleTest.php
@@ -13,7 +13,52 @@ class ExampleTest extends TestCase
      */
     public function testBasicExample()
     {
-        $this->visit('/')
-             ->see('Laravel 5');
+        $this->visit('/login')
+             ->see('Login');
     }
+	public function testLoginIWithOutEmail()
+    {
+          $this->visit('/login')
+			 ->type('a@a.com', 'email')
+			 ->press('Login')
+			 ->seePageIs('/login');
+    }
+	public function testLoginIWithEmail()
+    {
+          $this->visit('/login')
+			 ->type('a@a.com', 'email')
+			 ->type('123456', 'password')
+			 ->press('Login')
+			 ->seePageIs('/progress');
+    }
+		public function testRegisterWithUsername()
+    {
+          $this->visit('/register')
+			 ->type('t@t.com', 'email')
+			 ->press('Submit')
+			 ->seePageIs('/register');
+    }
+		public function testRegisterWithFull()
+    {
+          $this->visit('/register')
+			 ->type('aa', 'username')
+			 ->type('aa@a.com', 'email')
+			 ->type('123456', 'password')
+			 ->type('123456', 'repassword')
+			 ->press('Submit')
+			 ->seePageIs('/progress');
+    }
+		public function testCreatePlanrWithoutSth()
+		{
+			  $this->visit('/plan')
+				 ->type('aa', 'pname')
+				 ->type('description', 'pdescription')
+				 ->type('123456', 'pbudget')
+				 ->type('1', 'pmonth')
+				 ->press('Save')
+				 ->seePageIs('/plan');
+		}
+
+	
+	
 }
